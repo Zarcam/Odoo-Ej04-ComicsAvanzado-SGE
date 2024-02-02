7b6160dd48f19622c6ebd6f332e706e0a160d7db
Zarcam <zarcam.t@gmail.com>
2024-02-02 16:21:36 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Biblioteca Comics Avanzada",
+    'summary': "Gestionar comics (Version avanzada): ",
+    'description': """
+        Gestor de bibliotecas (Version avanzada)
+    """,
+
+    'application': True,
+    'author': "Segia García",
+    'website': "http://apuntesfpinformatica.es",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base'],
+
+    'data': [
+        
+    ],
+}
